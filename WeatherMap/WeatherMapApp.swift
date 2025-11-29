f0f504fd3bdb232639afb7ca388b2042964b8079
Ivan Buruikov <la2espade@andex.com>
2025-11-29 22:51:23 +0300

    Initial Commit

---
+++ WeatherMap/WeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMapApp.swift
+//  WeatherMap
+//
+//  Created by One Man Studio on 29.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
